7b6ed826743b8696def8a92db1496fde491b7949
hen3066 <henning.astrom-jonsson@edufalkenberg.se>
2025-03-12 10:54:14 +0100

    11

--- src/client.java
+++ src/client.java
@@ -16,7 +16,7 @@ public class client {
                 String serverMessage;
                 Boolean close = Boolean.FALSE;
                 while ((serverMessage = input.readLine()) != null) {
-                    if (!serverMessage.equals("SERVER_EXIT")) {
+                    if (serverMessage.equals("SERVER_EXIT")) {
                         close = Boolean.TRUE;
                         break;
                     }
